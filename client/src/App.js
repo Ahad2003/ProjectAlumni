6629267289dabc8bd4aae5d8dcb7f406240a1235
BeginnerCoder21 <anushka.war21@gmail.com>
2022-12-03 19:05:54 +0530

    files uploaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
-
+import { Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
-
+import { Link } from "react-router-dom";
 import { AuthProvider } from './context/auth';
 import AuthRoute from './util/AuthRoute';
 import MenuBar from './components/MenuBar';
@@ -15,18 +15,27 @@ import SinglePost from './pages/SinglePost';
 import Start from './pages/Start';
 
 function App() {
+  // let navigate = useNavigate(); 
+  // const routeChange = () =>{ 
+  //   let path = `newPath`; 
+  //   navigate(path);
+  // }
   return (
     <AuthProvider>
       <Router>
         <Container>
         <div className='body'>
+
           <div className="left">
             <span>ALUMNI INTERACTION PLATFORM</span>
             <h1>BENNE<span>TT</span></h1>
             <img className='image' src="https://imageio.forbes.com/specials-images/imageserve/62380c71af36178f0f91f59d/0x0.jpg?format=jpg&width=1200" alt="img" />
             <h1 className='next'>GRAM</h1>
           </div>
-
+          <div className="but">
+          <Button onClick={<Start/>}> Get Started </Button>
+          </div>
+          
           {/* <Start></Start> */}
         </div>
           {/* <MenuBar />
@@ -34,6 +43,7 @@ function App() {
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
           <Route exact path="/posts/:postId" component={SinglePost} /> */}
+          
         </Container>
       </Router>
     </AuthProvider>
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -2,18 +2,18 @@ import React from "react";
 import AuthRoute from '../util/AuthRoute';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
-
 import MenuBar from '../components/MenuBar';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import SinglePost from '../pages/SinglePost';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import SinglePost from './SinglePost';
 
 function Start(){
     return(
         <>
         <MenuBar />
           <Route exact path="/" component={Home} />
+          <Route path="/start" component={Start} />
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
           <Route exact path="/posts/:postId" component={SinglePost} />
