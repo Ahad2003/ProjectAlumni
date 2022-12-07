db43c71c6c8afa75a72ffd79aec95d5cdf229339
Vansh Sachdev <vksvs25@gmail.com>
2022-12-07 17:00:51 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import { Button } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css';
@@ -17,14 +15,36 @@ import SinglePost from './pages/SinglePost';
 import Start from './pages/Start';
 
 function App() {
+  // let navigate = useNavigate(); 
+  // const routeChange = () =>{ 
+  //   let path = `newPath`; 
+  //   navigate(path);
+  // }
   return (
     <AuthProvider>
       <Router>
-        <Route path="/" component={Start} />
-        <Route exact path="/home" component={Home} />
-        <AuthRoute exact path="/login" component={Login} />
-        <AuthRoute exact path="/register" component={Register} />
-        <Route exact path="/posts/:postId" component={SinglePost} />
+        <Container>
+        <div className='body'>
+
+          <div className="left">
+            <span>ALUMNI INTERACTION PLATFORM</span>
+            <h1>BENNE<span>TT</span></h1>
+            <img className='image' src="https://imageio.forbes.com/specials-images/imageserve/62380c71af36178f0f91f59d/0x0.jpg?format=jpg&width=1200" alt="img" />
+            <h1 className='next'>GRAM</h1>
+          </div>
+          <div className="but">
+          <Button onClick={<Start/>}> Get Started </Button>
+          </div>
+          
+          {/* <Start></Start> */}
+        </div>
+          {/* <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+          <Route exact path="/posts/:postId" component={SinglePost} /> */}
+          
+        </Container>
       </Router>
     </AuthProvider>
   );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { Grid, Transition } from 'semantic-ui-react';
-import MenuBar from '../components/MenuBar';
+
 import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
@@ -15,33 +15,30 @@ function Home() {
   } = useQuery(FETCH_POSTS_QUERY);
 
   return (
-    <>
-      <MenuBar />
-      <Grid columns={3}>
-        <Grid.Row className="page-title">
-          <h1>Recent Posts</h1>
-        </Grid.Row>
-        <Grid.Row>
-          {user && (
-            <Grid.Column>
-              <PostForm />
-            </Grid.Column>
-          )}
-          {loading ? (
-            <h1>Loading posts..</h1>
-          ) : (
-            <Transition.Group>
-              {posts &&
-                posts.map((post) => (
-                  <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                    <PostCard post={post} />
-                  </Grid.Column>
-                ))}
-            </Transition.Group>
-          )}
-        </Grid.Row>
-      </Grid>
-    </>
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
   );
 }
 
